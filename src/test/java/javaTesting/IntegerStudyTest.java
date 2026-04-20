429b0aae8bbc5a251b2b94ba653b8aac9db50730
Samuel <oluwamuyiwasamuel08@gmail.com>
2026-04-20 10:00:44 +0200

    Test 3.1: integerComparationStudy (using assertTrue())

--- src/test/java/javaTesting/IntegerStudyTest.java
+++ src/test/java/javaTesting/IntegerStudyTest.java
@@ -3,16 +3,27 @@ package javaTesting;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IntegerStudyTest {
 
     @Test
     public void integerStudy(){
-        Integer eight = new Integer(8);
         Integer thirteen = 13;
         int expected = 13;
 
         assertEquals(expected, thirteen.intValue(), "The number thirteen actually equals 13");
 
+    }
+
+    @Test
+    public void integerComparationStudy(){
+        Integer ocho = new Integer(8);
+        Integer eight = new Integer("8");
+
+        // assertTrue(ocho == eight, "The number 'ocho' equals the number 'eight'");
+        assertTrue(ocho.equals(eight), "The number 'ocho' equals the number 'eight'");
+        // assertTrue(condition) can be replaced with assertEquals(true, condition) i.e:
+        // assertEquals(true, ocho.equals(eight));
     }
 }
